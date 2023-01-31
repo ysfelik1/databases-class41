9db3f3db5dd9fc6a2ca97f5d06cda2ab666ddc2e
yusufelik <ysfelik1@gmail.com>
2023-01-31 01:18:04 +0100

    Added SQL commands for Invitee

--- Week1/exercise-1.js
+++ Week1/exercise-1.js
@@ -3,14 +3,28 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'hyfuser',
   password : 'hyfpassword',
-  database : 'userdb',
+  database : 'meetup',
 });
 
 connection.connect();
 
-connection.query('SELECT room_name as name from room', function (error, results, fields) {
+const createDatabase=' DROP DATABASE IF EXISTS meetup; CREATE DATABASE meetup;'
+const createTableInvitee=`DROP TABLE IF EXISTS invitee;
+CREATE TABLE invitee (
+   invitee_no INT  PRIMARY KEY AUTO_INCREMENT,
+   invitee_name  CHAR(100) NOT NULL,
+   invited_by INT NOT NULL
+ ); `
+ const insertInviteeData=` INSERT INTO   invitee ( invitee_name,  invited_by)VALUES ('Yusuf ELIK',1);
+ INSERT INTO   invitee ( invitee_name,  invited_by)VALUES ('Ali  ELIK',1);
+  INSERT INTO   invitee ( invitee_name,  invited_by)VALUES ('Scarlett JOHANSSON',1);
+   INSERT INTO   invitee ( invitee_name,  invited_by)VALUES ('Eva MENDEZ',1);
+    INSERT INTO   invitee ( invitee_name,  invited_by)VALUES ('Angelina JULIE',1);
+  `
+
+connection.query(createDatabase+createTableInvitee+insertInviteeData, function (error, results, fields) {
   if (error) throw error;
-  console.log('The solution is: ', results[1].name );
+  console.log('Queries completed');
 });
 
 connection.end();
