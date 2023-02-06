7709220067e30d2d38fc24f64f16655ae2cab34f
yusufelik <ysfelik1@gmail.com>
2023-02-06 23:03:33 +0100

    ex1 completed

--- Week2/ex-1.js
+++ Week2/ex-1.js
@@ -5,26 +5,29 @@ const connection = mysql.createConnection({
     password: 'hyfpassword',
     database: 'research',
     multipleStatements: true
-  });
+});
 
-  const createTableAuthors=`DROP TABLE  IF EXISTS authors;
+const createTableAuthors = `DROP TABLE  IF EXISTS authors;
   CREATE  TABLE authors (author_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
           author_name VARCHAR (50) NOT NULL,
           university VARCHAR (100),
           date_of_birth DATE,
           h_index INT,
           gender ENUM('Male','Female'))`;
-  const executeQuery = (myQuery) => {
+const alterTableAuthors = ` ALTER TABLE authors ADD COLUMN mentor_id INT, ADD FOREIGN KEY (mentor_id) REFERENCES authors(author_id);`;
 
-    connection.connect();
-    connection.query(myQuery, function (error, results) {
-        if (error) throw error;
-        console.log(results);
-    });
-    connection.end();
-  }
+connection.connect();
+connection.query(createTableAuthors, function (error) {
+    if (error) throw error;
+    console.log('Table created');
+});
+connection.query(alterTableAuthors, function (error) {
+    if (error) throw error;
+    console.log('New column added');
+});
 
-  executeQuery(createTableAuthors);
+connection.end();
 
 
- 
+
+
