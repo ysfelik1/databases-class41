8aca22042f59962a1f5882986d60531fec848db0
yusufelik <ysfelik1@gmail.com>
2023-02-21 00:12:35 +0100

    created a file for array

---
+++ Week4/homework/ex2-transactions/data.js
@@ -0,0 +1,69 @@
+export const accountsData = [
+    {
+      account_number: 101,
+      balance: 5000,
+      account_changes: [
+        {
+          change_number: 1,
+          amount: 3000,
+          changed_date: '02.01.2023',
+          remark: 'monthly payment',
+        },
+      ],
+    },
+    {
+      account_number: 102,
+      balance: 5000,
+      account_changes: [
+        {
+          change_number: 1,
+          amount: 3000,
+          changed_date: '02.01.2023',
+          remark: 'monthly payment',
+        },
+        {
+          change_number: 2,
+          amount: 1000,
+          changed_date: '02.20.2023',
+          remark: 'bonus',
+        },
+      ],
+    },
+    {
+      account_number: 103,
+      balance: 5000,
+      account_changes: [
+        {
+          change_number: 1,
+          amount: 3000,
+          changed_date: '02.01.2023',
+          remark: 'monthly payment',
+        },
+      ],
+    },
+    {
+      account_number: 104,
+      balance: 5000,
+      account_changes: [
+        {
+          change_number: 1,
+          amount: 3000,
+          changed_date: '02.01.2023',
+          remark: 'monthly payment',
+        },
+      ],
+    },
+    {
+      account_number: 105,
+      balance: 5000,
+      account_changes: [
+        {
+          change_number: 1,
+          amount: 3000,
+          changed_date: '02.01.2023',
+          remark: 'monthly payment',
+        },
+      ],
+    },
+  ];
+ 
