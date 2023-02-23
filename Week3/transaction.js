a5490ef0944c8ff4d66c3af3506cebb256efeca7
yusufelik <ysfelik1@gmail.com>
2023-02-23 17:09:03 +0100

    feedback corrections

--- Week3/transaction.js
+++ Week3/transaction.js
@@ -7,21 +7,32 @@ const connection = mysql.createConnection({
     multipleStatements: true
 });
 
-const transferAmount  = `START TRANSACTION;
+const transferAmount = (senderAccountNumber, receiverAccountNumber, amount, date, remark) => {
+
+    const transactionQuery = `START TRANSACTION;
 UPDATE account
-SET balance=balance-1000
-WHERE account_number=101;
+SET balance=balance-${amount}
+WHERE account_number=${senderAccountNumber};
 UPDATE account
-SET balance=balance+1000
-WHERE account_number=102 ;
+SET balance=balance+${amount}
+WHERE account_number=${receiverAccountNumber} ;
 INSERT INTO  account_changes (account_number,amount,changed_date,remark)
-values(102,1000,'2023-02-13','e-bike payment');
+values(${receiverAccountNumber},${amount},'${date}','${remark}');
 INSERT INTO  account_changes (account_number,amount,changed_date,remark)
-values(101,-1000,'2023-02-13','e-bike payment');
+values(${senderAccountNumber},-${amount},'${date}','${remark}');
 COMMIT;`;
 
+    return transactionQuery;
+}
+
+const objectDate = new Date();
+const day = objectDate.getDate();
+const month = objectDate.getMonth();
+const year = objectDate.getFullYear();
+
+
 connection.connect();
-connection.query(transferAmount, function (error) {
+connection.query(transferAmount(101, 102, 1000, `${year}-${month}-${day}`, `e-bike`), function (error) {
     if (error) throw error;
     console.log('Transfer succeed');
 });
